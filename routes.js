02932591e41945d7b773bfaa99ca97b3987ba870
Alex <pettigrc@oregonstate.edu>
2025-08-03 10:50:05 -0700

    removed unneccessary import

--- routes.js
+++ routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 const mongoclient = require('./db');
 
 // Gets adds a book to the history database
